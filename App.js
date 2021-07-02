1f2b9c732d28b16d2500f0e9179818ddd68ed0c5
yongho1212 <yongho6800@gmail.com>
2021-07-02 18:29:03 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,82 @@
+import "react-native-gesture-handler";
+
+// Import React and Component
+import React from "react";
+
+// Import Navigators from React Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// Import Screens
+import Splash from "./screens/Splash";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
+import Home from "./screens/Home";
+
+const Stack = createStackNavigator();
+
+const Auth = () => {
+  // Stack Navigator for Login and Sign up Screen
+  return (
+    <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Screen
+        name="LoginScreen"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="RegisterScreen"
+        component={Register}
+        options={{
+          title: "Register", //Set Header Title
+          headerStyle: {
+            backgroundColor: "#307ecc", //Set Header color
+          },
+          headerTintColor: "#fff", //Set Header text color
+          headerTitleStyle: {
+            fontWeight: "bold", //Set Header text style
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+/* Main Navigator */
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen">
+        {/* SplashScreen which will come once for 2 Seconds */}
+        <Stack.Screen
+          name="SplashScreen"
+          component={Splash}
+          // Hiding header for Splash Screen
+          options={{ headerShown: false }}
+        />
+        {/* Auth Navigator which include Login Signup */}
+        <Stack.Screen
+          name="Auth"
+          component={Auth}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={Home}
+          options={{
+            title: "Home", //Set Header Title
+            headerStyle: {
+              backgroundColor: "#307ecc", //Set Header color
+            },
+            headerTintColor: "#fff", //Set Header text color
+            headerTitleStyle: {
+              fontWeight: "bold", //Set Header text style
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ screens/Home.js
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+
+import auth from "@react-native-firebase/auth";
+
+const HomeScreen = ({ navigation }) => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((user) => {
+      console.log("user", JSON.stringify(user));
+      setUser(user);
+    });
+
+    return subscriber;
+  }, []);
+
+  const logout = () => {
+    Alert.alert(
+      "Logout",
+      "Are you sure? You want to logout?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => {
+            return null;
+          },
+        },
+        {
+          text: "Confirm",
+          onPress: () => {
+            auth()
+              .signOut()
+              .then(() => navigation.replace("Auth"))
+              .catch((error) => {
+                console.log(error);
+                if (error.code === "auth/no-current-user")
+                  navigation.replace("Auth");
+                else alert(error);
+              });
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 16 }}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 20,
+              textAlign: "center",
+              marginBottom: 16,
+            }}
+          >
+            Firebase Auth
+          </Text>
+          {user ? (
+            <Text>
+              Welcome{" "}
+              {user.displayName
+                ? user.displayName
+                : user.email}
+            </Text>
+          ) : null}
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            activeOpacity={0.5}
+            onPress={logout}
+          >
+            <Text style={styles.buttonTextStyle}>
+              Logout
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <Text
+          style={{
+            fontSize: 18,
+            textAlign: "center",
+            color: "grey",
+          }}
+        >
+          React Native Firebase Authentication
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            textAlign: "center",
+            color: "grey",
+          }}
+        >
+          www.aboutreact.com
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    minWidth: 300,
+    backgroundColor: "#7DE24E",
+    borderWidth: 0,
+    color: "#FFFFFF",
+    borderColor: "#7DE24E",
+    height: 40,
+    alignItems: "center",
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 25,
+  },
+  buttonTextStyle: {
+    color: "#FFFFFF",
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+});
